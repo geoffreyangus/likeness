1ef2cb575f4771baa7ae494a8b3d20fdce90d325
Geoffrey L. Angus <glangus@us.ibm.com>
2017-10-25 18:33:26 -0700

    added celebrity photos with metadata

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # likeness
 An application that predicts post popularity from the features of a given Instagram post.
+
+Notes:
+
+10/25
+- Scraped the most recent ~50 photos of the top 40 instagram celebrities using `instagram-scraper`. Ignored @taylorswift because she only had 25 photos. Sad.
+	- Source: https://github.com/rarcega/instagram-scraper
+- We say we have approximately 50 photos because we are excluding videos.
--- util.py
+++ util.py
@@ -1,1 +1,30 @@
-print 'test'
+import json
+
+usernameList = open('ig_users.txt').read().splitlines()
+
+# Gets all captions given a user's username.
+def getCaptions(username):
+	results = []
+
+	with open('users/'+username+'/'+username+'.json') as json_data:
+	    d = json.load(json_data)
+	    for pic in d:
+	    	if pic and pic['caption'] and pic['caption']['text']:
+	    		results.append(pic['caption']['text'])
+
+	return results
+
+def getAverageLikes(username):
+	sum = 0
+	count = 0
+	with open('users/'+username+'/'+username+'.json') as json_data:
+	    d = json.load(json_data)
+	    for pic in d:
+	    	if pic and pic['likes'] and pic['likes']['count']:
+	    		sum += pic['likes']['count']
+	    		count += 1
+	return float(sum) / float(count)
+
+
+for username in usernameList:
+	print username, getAverageLikes(username)
